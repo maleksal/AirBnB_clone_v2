5f733c4b11a50d87229efa33984ab7f1c06cb52e
Malek Salem <malek.salem.14@gmail.com>
2020-08-12 18:40:10 +0100

    fix some bugs

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-""" """
+""" Test Amenity"""
 from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 
 
 class test_Amenity(test_basemodel):
-    """ """
+    """ Test Amenity Class """
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """ Constructor """
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """ test for data type"""
         new = self.value()
         self.assertEqual(type(new.name), str)
