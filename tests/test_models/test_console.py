2fbb6a3dbeb80a04dc956b94c5c4f7bc696f2414
Malek Salem <malek.salem.14@gmail.com>
2020-08-14 23:30:56 +0100

    update unittests

--- tests/test_models/test_console.py
+++ tests/test_models/test_console.py
@@ -22,25 +22,58 @@ class TestConsole(unittest.TestCase):
         except:
             pass
 
-    def test_create_method_work_valid_args(self):
-        """ test create method with valid args"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("create Place")
-        msg = f.getvalue()[:-1]
-        self.assertRegex(msg, '\w{8}-\w{4}-\w{4}-\w{4}-\w{12}')
+    def test_pep8(self):
+        """check for pep8"""
+        style = pep8.StyleGuide(quiet=True)
+        p = style.check_files(["console.py"])
+        self.assertEqual(p.total_errors, 0, "fix Pep8")
 
-    def test_create_method(self):
-        """ test create method with parameter name"""
+    def test_docstrings(self):
+        """Check for docstrings."""
+        self.assertIsNotNone(HBNBCommand.__doc__)
+        self.assertIsNotNone(HBNBCommand.emptyline.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_quit.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_EOF.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_create.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_show.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_destroy.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_all.__doc__)
+        self.assertIsNotNone(HBNBCommand.do_update.__doc__)
+        self.assertIsNotNone(HBNBCommand.count.__doc__)
+        self.assertIsNotNone(HBNBCommand.strip_clean.__doc__)
+        self.assertIsNotNone(HBNBCommand.default.__doc__)
 
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("create Place name=\"UK\"")
-            HBNBCommand().onecmd("all Place")
-        msg = f.getvalue()[:-1]
-        self.assertRegex(msg, "'place': 'UK'")
+    def test_create_non_valid_args(self):
+        """test create command with non-valid args"""
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("create")
+            self.assertEqual(
+                "** class name missing **\n", f.getvalue())
 
-    def test_create_method_without_args(self):
-        """ test create method without args"""
-        with patch('sys.stdout', new=StringIO()) as f:
-            HBNBCommand().onecmd("create")
-        msg = f.getvalue()[:-1]
-        self.assertEqual(msg, "** class name missing **")
+    @unittest.skipIf(type(models.storage) == DBStorage, "Testing DBStorage")
+    def test_create(self):
+        """create command."""
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("create BaseModel")
+            bm = f.getvalue().strip()
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("create User")
+            us = f.getvalue().strip()
+
+    @unittest.skipIf(type(models.storage) == DBStorage, "Testing DBStorage")
+    def test_create_kwargs(self):
+        """create command with kwargs attr='value'."""
+        with patch("sys.stdout", new=StringIO()) as f:
+            call = ('create Place city_id="0002" name="somthing" '
+                    'number_rooms=10 latitude=50.77 longitude=5')
+            self.HBNB.onecmd(call)
+            pl = f.getvalue().strip()
+        with patch("sys.stdout", new=StringIO()) as f:
+            self.HBNB.onecmd("all Place")
+            output = f.getvalue()
+            self.assertIn(pl, output)
+            self.assertIn("'city_id': '0001'", output)
+            self.assertIn("'name': 'My house'", output)
+            self.assertIn("'number_rooms': 4", output)
+            self.assertIn("'latitude': 37.77", output)
+            self.assertNotIn("'longitude'", output)
