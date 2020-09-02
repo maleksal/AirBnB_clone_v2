1233441fa5efcce1eb1fb1f6028ca6c3aec8136c
Malek Salem <malek.salem.14@gmail.com>
2020-09-02 14:50:25 +0100

    add some logic to state model

--- models/state.py
+++ models/state.py
@@ -2,6 +2,7 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, DateTime, String, ForeignKey
+from sys import getenv
 
 
 class State(BaseModel, Base):
@@ -11,3 +12,15 @@ class State(BaseModel, Base):
         String(128),
         nullable=False
         )
+
+    if getenv("HBNB_TYPE_STORAGE") != "db":
+        @property
+        def city_getter(self):
+            from models import storage
+            from models.city import City
+
+            hold_objects = []
+            for value in storage.all(City).values():
+                if value.state_id == self.id:
+                    hold_objects.append(value)
+            return hold_objects
